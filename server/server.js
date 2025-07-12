ad54d9829ecf1545e9f6fde815b2485f7c3abc6d
Pari Sharma <sharmapari10004@gmail.com>
2025-07-13 00:51:59 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import env from "dotenv";
 const app = express();
 const port = 3000;
 const corsOptions = {
-  origin:["http://localhost:5173"],
+  origin:["https://keeper-app-frontend-fzyo.onrender.com"],
 };
 env.config();
 
